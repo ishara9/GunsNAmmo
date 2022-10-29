c53d85733a437ee908e18309501a9e0cb1afba8e
Ishara Abeysekera <ishara.abeysekera@endowus.com>
2022-10-29 17:09:03 +0530

    repo test

--- src/test/java/com/zpybotlabs/gunsnammo/repository/GunsRepositoryTest.java
+++ src/test/java/com/zpybotlabs/gunsnammo/repository/GunsRepositoryTest.java
@@ -1,8 +1,40 @@
 package com.zpybotlabs.gunsnammo.repository;
 
+import com.zpybotlabs.gunsnammo.model.Gun;
+import java.util.List;
+import javax.transaction.Transactional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Transactional //default rollback
 class GunsRepositoryTest {
 
-  //Tests for custom methods
+  @Autowired
+  private GunsRepository gunsRepository;
+
+  @Test
+  @Order(1)
+  public void testAdd() {
+    Gun gun = new Gun();
+    gun.setGunId(1L);
+    Gun gunResp = gunsRepository.save(gun);
+    Assertions.assertNotNull(gunResp);
+  }
+
+  @Test
+  @Order(2)
+  public void testFindAll() {
+    List<Gun> guns = gunsRepository.findAll();
+    Assertions.assertEquals(0, guns.size());
+  }
+
 
 
 }
